830bdb863bab51d2ca6ca8e64da48bcfb0073b2a
KaydenLee03 <03kayden.lee@kamsc.org>
2025-10-20 13:36:58 -0400

    exercise

--- src/main/java/GuessingGame.java
+++ src/main/java/GuessingGame.java
@@ -1,11 +1,27 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class GuessingGame {
     public static void main(String[] args) {
-        // TODO: Prompt the user with a welcome message
-        // TODO: Generate a random secret number between 1 and 100 (inclusive)
-        // TODO: Use a do...while loop to read guesses until correct
-        // TODO: Print "Too low" / "Too high" / "You got it!" accordingly
-        // TODO: Use Scanner for input
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int correctNum = random.nextInt(100);
+        int guess;
+
+
+        System.out.println("Welcome to the Guessing Game!\n" +
+                "I'm thinking of a number between 1 and 100.");
+
+        do {
+            System.out.println("Enter your guess: ");
+            guess = scan.nextInt();
+            if (guess < correctNum) {
+                System.out.println("Too low! Try again.");
+            }
+            else {
+                System.out.println("Too high! Try again");
+            }
+        } while (guess != correctNum);
+        System.out.println("You got it!");
     }
 }
--- src/main/java/ShoppingCart.java
+++ src/main/java/ShoppingCart.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class ShoppingCart {
     public static void main(String[] args) {
-        // TODO: Ask the user how many items they are purchasing
-        // TODO: Use a for loop to read each item's price (double)
-        // TODO: Accumulate the total cost
-        // TODO: Print the final summary line with item count and total
-        // TODO: Use Scanner for input
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many items are you purchasing? ");
+        int numItems = scan.nextInt();
+        double total = 0;
+
+        for (int i = 1; i <= numItems; i++) {
+            System.out.print("Price of item " + i + "? ");
+            total += scan.nextDouble();
+        }
+        System.out.println("Your cart has " + numItems + " items with a total cost of $" + total);
     }
 }
